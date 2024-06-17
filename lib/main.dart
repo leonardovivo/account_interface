debdb689c00cbc5984531b9bc7d7e58c70081340
leonardovivo <leonardovivo3@gmail.com>
2024-06-16 21:56:30 -0300

    Arquivo main do projeto completo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:interface_conta/screens/register_screen.dart';
+import 'screens/register_screen.dart';
 import 'screens/welcome_screen.dart';
 import 'screens/login_screen.dart';
 
